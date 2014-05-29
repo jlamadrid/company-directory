cc92cd250df82f0d0c5595faf0f282e53aa1d438
Juan Lamadrid <juan.lamadrid@gmail.com>
2014-05-29 09:18:40 -0400

    generate model using rails generate model Employee fname:string lname:string manager_id:integer title:string department:string phone_cell:string phone_home:string phone_office:string email:string tags:string -s

---
+++ db/migrate/20140529131658_create_employees.rb
@@ -0,0 +1,18 @@
+class CreateEmployees < ActiveRecord::Migration
+  def change
+    create_table :employees do |t|
+      t.string :fname
+      t.string :lname
+      t.integer :manager_id
+      t.string :title
+      t.string :department
+      t.string :phone_cell
+      t.string :phone_home
+      t.string :phone_office
+      t.string :email
+      t.string :tags
+
+      t.timestamps
+    end
+  end
+end

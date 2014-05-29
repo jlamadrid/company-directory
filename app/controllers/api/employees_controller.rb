c4e18ddd10c19db4f9b7d037834f8d8178986dd3
Juan Lamadrid <juan.lamadrid@gmail.com>
2014-05-29 10:40:35 -0400

    implement rest endpoint to show employee by id -> http://localhost:3000/api/employees/1

--- app/controllers/api/employees_controller.rb
+++ app/controllers/api/employees_controller.rb
@@ -2,11 +2,26 @@ module Api
 
   class EmployeesController < ApplicationController
 
+    before_action :set_employee, only: [:show]
+
+    #GET http://server:port/api/employees
     def index
 
       @employees = Employee.all
 
       render :json => { :success => true, :data =>  @employees }
+    end
+
+    #GET http://server:port/api/employees/1
+    def show
+      render :json => { :success => true, :data => @employee }
+    end
+
+
+    private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_employee
+      @employee = Employee.find(params[:id])
     end
 
   end
